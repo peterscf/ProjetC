560c921b35ccc34984fc817e3a568fc6154ee96f
Floriant PETERSCHMITT <floriant.peterschmitt@gmail.com>
2016-10-13 17:14:02 +0200

    ajout fichier ./doc/version_compilateur ajout./src/command_lib/vhdllib.cpp: création de library physique(dossier)

---
+++ src/commande_lib/vhdllib.cpp
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+/*Author: Floriant PETERSCHMITT
+/*Project: synthetiseur VHDL
+/*Date: 13/10/2016
+/*
+//////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////*/
+
+
+
+int main (int argc, char *argv[])
+
+//test présence nom de la library
+if (argc==0){
+	cout	<<"Error: too few arguments !" << endl
+		<<"example:\t\"vhdllib <name of physical library>"
+		<< endl;
+}
+else if(argc<1){
+	cout 	<<"Error: too much argument !" << endl
+		<<"example:\t\"vhdllib <name of physical library>"
+		<< endl;
+}
+else {
+	if (/*test path ok ?*/){
+	
+	}
+}
+
+return 0;
+}
