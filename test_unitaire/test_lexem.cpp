cdbd216e0b62fa3c82ecf517e667157a4a7b7da5
Floriant PETERSCHMITT <floriant.peterschmitt@gmail.com>
2016-11-17 15:02:48 +0100

    add lexem.h add test_lexem

---
+++ test_unitaire/test_lexem.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <sstream>
+#include<iostream>
+
+#include"/src/Lexem.h"
+
+using namespace std;
+
+int main(void){
+	
+	Lexem lex1("HELLO", "mot", 1, 2);
+	
+	cout 	<<"nom: "<< lex1.get_nom << endl
+		<<"type: "<< lex1.get_type<<endl
+		<<"position: "<< lex1.get_line_pos()<<","<< lex1.get_col_pos()<<endl;
+		
+}
