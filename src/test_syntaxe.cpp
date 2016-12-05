a488a626afa251518efbf1643f35d3415ddbb7fc
floriant <floriant.peterschmitt@gmail.com>
2016-12-05 14:47:43 +0100

    .

--- src/test_syntaxe.cpp
+++ src/test_syntaxe.cpp
@@ -10,26 +10,34 @@
 using namespace std;
 
 bool test_syntaxe(lexem lex);
+bool test_alphanumeric_et_underscore(string m);
 //void type_lexem (Lexem lex);
 
 int main(void){
-	string mot="ABC";
-	lexem lex1(">=");
+	string mot="abcd_12";
+	lexem lex1(mot);
 	Dictionnaire Dico("lexem_type.dico");
-		
-	Dico.affiche_vect();
-	
-	cout<< (mot.front()) <<endl;
 	
 	cout<<"nom: "<<lex1.get_nom()<<endl;
-	cout<<"type: "<< lex1.get_type()<<endl;
-	cout<<"position: "<< lex1.get_line_pos()<<","<< lex1.get_col_pos()<<endl;
+		
+	//Dico.affiche_vect();
+	if(test_syntaxe(lex1)==true){
+		cout <<"test lexical retourne vrai"<<endl;
+		cout<<"nom: "<<lex1.get_nom()<<endl;
+		cout<<"type: "<< lex1.get_type()<<endl;
+		cout<<"position: "<< lex1.get_line_pos()<<","<< lex1.get_col_pos()<<endl;
+	}
+	else {
+		cout<<"erreur de syntaxe !!!"<<endl;
+	}
 	
+
+	/*
 	Dico.Type(&lex1);
 	
 	cout<<"nom: "<<lex1.get_nom()<<endl;
 	cout<<"type: "<< lex1.get_type()<<endl;
-	cout<<"position: "<< lex1.get_line_pos()<<","<< lex1.get_col_pos()<<endl;
+	cout<<"position: "<< lex1.get_line_pos()<<","<< lex1.get_col_pos()<<endl;*/
 		return 0;
 }
 
@@ -44,24 +52,40 @@ Le premier caractère du lexème doit être une lettre
 Un caractère spécial est un lexème
 Un lexème ne peut contenir un autre lexème
 */
-/*
+
 bool test_syntaxe(lexem lex){
 	string mot=lex.get_nom();
-	bool out = true;
-
+	bool out = false;
 	//regle de syntaxe
-	if (mot.find("_")==0 || mot.find("_")==mot.length()){
+	if (mot.find("_")==0 || mot.find("_")==(mot.size()-1)){
 		out=false;
 	}	
-	else if (mot.front() < 'a' && mot.front() > 'z'){
+	else if (mot.at(0) <= 'a' && mot.at(0) >= 'z'){
 		out=false;
 	}
+	else if (mot.at((mot.size()-1)) == '_'){
+		out=false;
+	}
+	else if (test_alphanumeric_et_underscore(mot) != true){
+		out=false;
+	}
+	else{
+		out = true;
+	}
 		return out;
-}*/
-//////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////
-/*void type_lexem(lexem lex){
-
 }
 
-*/
+bool test_alphanumeric_et_underscore(string m){
+	bool out;
+	int i=0;
+	while(/*i<m.size()*/ m.at(i) == '\0' /*|| out!=false*/){
+		if ((m.at(i) != '_') || (m.at(i) < 'a' && m.at(i) > 'z') || (m.at(i)<'0' && m.at(i)>'9')){
+			out=false;
+		}
+		i++;	
+	}
+}
+//Fin programme
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
