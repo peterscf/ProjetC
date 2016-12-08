e821d8d9e44a1d3aace90bb752e7a2d62af174c0
peterscf <floriant.peterschmitt@gmail.com>
2016-12-08 15:07:29 +0100

    ajout de la fonction lexical test .h et cpp

--- src/test_syntaxe.cpp
+++ src/test_syntaxe.cpp
@@ -3,88 +3,49 @@
 #include<string>
 #include"lexem.h"
 #include"Dictionnaire.h"
+#include"lexem.h"
+#include"lexical_funct.h"
 
 #include <iostream>
 #include <string>
 
 using namespace std;
-
-bool test_syntaxe(lexem lex);
-bool test_alphanumeric_et_underscore(string m);
+void affiche_resultat_test_lexical(lexem lex);
 //void type_lexem (Lexem lex);
 
 int main(void){
-	string mot="abcd_12";
-	lexem lex1(mot);
-	Dictionnaire Dico("lexem_type.dico");
+	lexem lex1("az_09");
+	lexem lex2("ab*d_12");
+	lexem lex3("____ADC999'");
+	lexem lex4("ABC_");
+	//Dictionnaire Dico("lexem_type.dico");
 	
 	cout<<"nom: "<<lex1.get_nom()<<endl;
-		
-	//Dico.affiche_vect();
-	if(test_syntaxe(lex1)==true){
-		cout <<"test lexical retourne vrai"<<endl;
-		cout<<"nom: "<<lex1.get_nom()<<endl;
-		cout<<"type: "<< lex1.get_type()<<endl;
-		cout<<"position: "<< lex1.get_line_pos()<<","<< lex1.get_col_pos()<<endl;
-	}
-	else {
-		cout<<"erreur de syntaxe !!!"<<endl;
-	}
+	cout<<"nom: "<<lex2.get_nom()<<endl;
+	cout<<"nom: "<<lex3.get_nom()<<endl;
+	cout<<"nom: "<<lex4.get_nom()<<endl;
 	
-
-	/*
-	Dico.Type(&lex1);
+	cout << endl <<endl;
 	
-	cout<<"nom: "<<lex1.get_nom()<<endl;
-	cout<<"type: "<< lex1.get_type()<<endl;
-	cout<<"position: "<< lex1.get_line_pos()<<","<< lex1.get_col_pos()<<endl;*/
+	affiche_resultat_test_lexical(lex1);
+	affiche_resultat_test_lexical(lex2);
+	affiche_resultat_test_lexical(lex3);
+	affiche_resultat_test_lexical(lex4);
 		return 0;
 }
+void affiche_resultat_test_lexical(lexem lex){
+	if(test_lexical(lex)){
+		cout <<"test lexical retourne vrai"<<endl;
+		cout<<"nom: "<<lex.get_nom()<<endl;
+		cout<<"type: "<< lex.get_type()<<endl;
+		cout<<"position: "<< lex.get_line_pos()<<","<< lex.get_col_pos()<<endl<<endl;
+	}
+	else {
+		cout<<"erreur lexical !!!"<< endl << lex.get_nom()<<endl<<endl;
+	}
 
-
-//////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////
-/*Règles d’orthographe des lexèmes en VHDL :
-Un lexème peut être composé de caractères alphanumériques et d’underscore(s)
-Les underscore(s) peuvent être utilisés dans un lexème à condition qu’ils ne soient 
-ni le premier, ni le dernier caractère de celui-ci
-Le premier caractère du lexème doit être une lettre
-Un caractère spécial est un lexème
-Un lexème ne peut contenir un autre lexème
-*/
-
-bool test_syntaxe(lexem lex){
-	string mot=lex.get_nom();
-	bool out = false;
-	//regle de syntaxe
-	if (mot.find("_")==0 || mot.find("_")==(mot.size()-1)){
-		out=false;
-	}	
-	else if (mot.at(0) <= 'a' && mot.at(0) >= 'z'){
-		out=false;
-	}
-	else if (mot.at((mot.size()-1)) == '_'){
-		out=false;
-	}
-	else if (test_alphanumeric_et_underscore(mot) != true){
-		out=false;
-	}
-	else{
-		out = true;
-	}
-		return out;
 }
 
-bool test_alphanumeric_et_underscore(string m){
-	bool out;
-	int i=0;
-	while(/*i<m.size()*/ m.at(i) == '\0' /*|| out!=false*/){
-		if ((m.at(i) != '_') || (m.at(i) < 'a' && m.at(i) > 'z') || (m.at(i)<'0' && m.at(i)>'9')){
-			out=false;
-		}
-		i++;	
-	}
-}
 //Fin programme
 //////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////
