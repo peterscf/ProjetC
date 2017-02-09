4cf1b2a8cf107d43f951dc7d1525c25c5b3f3935
Floriant PETERSCHMITT <floriant.peterschmitt@gmail.com>
2017-02-09 08:11:48 +0100

    ajout des test examen

---
+++ src/EXAM_APP/entite_seule.vhd
@@ -0,0 +1,10 @@
+library STD;
+  use STD.textio.all;
+library IEEE;
+  use IEEE.bit_1164.all;
+  use IEEE.bit_misc.all;
+  use IEEE.bit_arith.all;
+
+entity entite_sans_portes is
+ end entite_sans_portes;
+
